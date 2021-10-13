8800c46a8958c3722fe8a35f68be3403d13e21be
Tanencito <67567683+Tanencito@users.noreply.github.com>
2021-10-13 16:05:30 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,25 @@
+# :cloud: Computación en la nube: Trabajo Práctico 3
+
+## :bust_in_silhouette: Datos personales
+
+- Nombre: **Daniel Dumont**
+- Legajo: **43848**
+
+## :crystal_ball: Instalación
+
+### Correr proyecto
+
+```bash
+Primero borramos todas las imagenes que existan en Docker:
+docker container rm dynamodb
+
+Después creamos la red:
+docker network create awslocal
+
+Ahora descargamos la imagen del shell y la compartimos para que la API se pueda comunicar con la tabla:
+docker run --rm -p 8000:8000 --network awslocal --name dynamodb amazon/dynamodb-local:1.15.0 -jar DynamoDBLocal.jar -sharedDb
+
+Por último, ingresamos a la carpeta del proyecto por medio del panel de comandos y ejecutamos lo siguiente para levantar la API:
+sam local start-api --docker-network awslocal
+```
+
---
+++ index.js
@@ -0,0 +1,105 @@
+const { v4: uuidv4 } = require('uuid');
+var AWS = require('aws-sdk');
+
+var handler = async({ pathParameters, httpMethod, body }) => {
+
+    var dynamodb = new AWS.DynamoDB({
+        apiVersion: '2012-08-10',
+        endpoint: 'http://dynamodb:8000',
+        region: 'us-west-2',
+        credentials: {
+            accessKeyId: '2345',
+            secretAccessKey: '2345'
+        }
+    });
+
+    var docClient = new AWS.DynamoDB.DocumentClient({
+        apiVersion: '2012-08-10',
+        service: dynamodb
+    });
+
+    let re = new RegExp('^/envios.*')
+        // codigo de la funcion
+    switch (httpMethod) {
+
+        case 'PUT':
+            const idEntrega = (pathParameters || {}).idEntrega || false;
+            if (idEntrega) {
+                const updateParams = {
+                    TableName: 'Envio',
+                    Key: {
+                        id: idEntrega
+                    },
+                    UpdateExpression: 'REMOVE pendiente',
+                    ConditionExpression: 'attribute_exists(pendiente)'
+                }
+
+                try {
+                    await docClient.update(updateParams).promise()
+                    return {
+                        statusCode: 200,
+                        headers: { "content-type": "text/plain" },
+                        body: `${idEntrega} Entregado`
+                    };
+                } catch {
+                    return {
+                        statusCode: 500,
+                        headers: { "content-type": "text/plain" },
+                        body: `No fue posible entregar el envio`
+                    };
+                }
+            } else { return { body: "no se encontro id" } }
+
+        case 'GET':
+            const params = {
+                TableName: 'Envio',
+                IndexName: 'EnviosPendientesIndex'
+            };
+
+            try {
+                const envios = await docClient.scan(params).promise()
+                return {
+                    statusCode: 200,
+                    headers: { "content-type": "application/json" },
+                    body: JSON.stringify(envios)
+                }
+            } catch (err) {
+                console.log(err)
+                return {
+                    statusCode: 500,
+                    headers: { "content-type": "text/plain" },
+                    body: 'Error get envios'
+                };
+            }
+
+        case 'POST':
+            console.log("entra post")
+            const createParams = {
+                TableName: 'Envio',
+                Item: {
+                    id: uuidv4(),
+                    fechaAlta: new Date().toISOString(),
+                    ...JSON.parse(body),
+                    pendiente: new Date().toISOString(),
+                }
+            }
+
+            try {
+                await docClient.put(createParams).promise()
+                return {
+                    statusCode: 200,
+                    headers: { "content-type": "application/json" },
+                    body: JSON.stringify(createParams.Item)
+                };
+            } catch {
+                return {
+                    statusCode: 500,
+                    headers: { "content-type": "text/plain" },
+                    body: 'Error creando envio'
+                };
+            }
+    }
+
+}
+
+exports.handler = handler;
